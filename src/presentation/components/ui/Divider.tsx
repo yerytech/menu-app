1df85645821ee803209fc33d5d98f3a43426e447
yerytech <yerytecnology@gmail.com>
2025-01-21 10:58:07 -0400

    Refactor Divider component to enhance styling and improve background color

--- src/presentation/components/ui/Divider.tsx
+++ src/presentation/components/ui/Divider.tsx
@@ -1,19 +1,23 @@
 import { StyleProp, View, ViewStyle } from "react-native";
+import { colors } from "../../../config/theme/theme";
 interface Props {
   style?: StyleProp<ViewStyle>;
 }
 export const Divider = ({ style }: Props) => {
   return (
-    <View
-      style={[
-        {
-          borderBottomWidth: 1,
-          borderBottomColor: "green",
-          // marginHorizontal: 10,
-          opacity: 0.5,
-        },
-        style,
-      ]}
-    />
+    <View style={{ backgroundColor: colors.cardBackground }}>
+      <View
+        style={[
+          {
+            borderRadius: 100,
+            borderBottomWidth: 1,
+            borderBottomColor: "gray",
+            marginHorizontal: 10,
+            opacity: 0.5,
+          },
+          style,
+        ]}
+      />
+    </View>
   );
 };
