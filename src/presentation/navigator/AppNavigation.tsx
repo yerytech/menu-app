c029ddd8abfd0539af68d4c4e2cda0492a14ccf7
yerytech <yerytecnology@gmail.com>
2025-01-21 11:36:11 -0400

    Add AlertScreen component and integrate it into AppNavigation

--- src/presentation/navigator/AppNavigation.tsx
+++ src/presentation/navigator/AppNavigation.tsx
@@ -3,6 +3,7 @@ import { HomeScreen } from "../screens/home/HomeScreen";
 import { Animation101Screen } from "../screens/animations/Animation101Screen";
 import { Animation102Screen } from "../screens/animations/Animation102Screen";
 import { SwitchScreen } from "../screens/switchs/SwitchScreen";
+import { AlertScreen } from "../screens/alerts/AlertScreen";
 const Stack = createStackNavigator();
 export const AppNavigation = () => {
   return (
@@ -25,6 +26,11 @@ export const AppNavigation = () => {
       <Stack.Screen
         name="SwitchScreen"
         component={SwitchScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="AlertScreen"
+        component={AlertScreen}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
---
+++ src/presentation/screens/alerts/AlertScreen.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export const AlertScreen = () => {
+  return (
+    <View>
+      <Text>AlertScreen</Text>
+    </View>
+  );
+};
