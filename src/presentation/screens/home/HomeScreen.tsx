2d2b4ce2521f2560eb8db683298b7413bc8bf6bb
yerytech <yerytecnology@gmail.com>
2025-01-23 20:12:50 -0400

    feat: replace View with CustomView in HomeScreen for improved layout

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -6,57 +6,58 @@ import { menuItems } from "../../../helper/menu-items";
 import { MenuItem } from "../../components/ui/MenuItem";
 import { animationItems } from "../../../helper/animation-items";
 import { uiMenuItems } from "../../../helper/uiMenuItems";
-
+import { CustomView } from "../../components/ui/CustomView";
 
 export const HomeScreen = () => {
   return (
-    <View style={globalStyles.mainContainer}>
-      <View style={globalStyles.globalMargin}>
-        <ScrollView>
-          <Title
-            text="Home"
-            safe
-          />
-          <View style={{ marginTop: 20 }}>
-            {animationItems.map((item, index) => (
-              <MenuItem
-                key={item.component}
-                {...item}
-                isFirst={index === 0}
-                isLast={index === animationItems.length - 1}
-                icon={item.icon}
-                component={item.component}
-              />
-            ))}
-          </View>
+    <CustomView
+      margin
+      style={globalStyles.mainContainer}
+    >
+      <ScrollView>
+        <Title
+          text="Home"
+          safe
+        />
+        <View style={{ marginTop: 20 }}>
+          {animationItems.map((item, index) => (
+            <MenuItem
+              key={item.component}
+              {...item}
+              isFirst={index === 0}
+              isLast={index === animationItems.length - 1}
+              icon={item.icon}
+              component={item.component}
+            />
+          ))}
+        </View>
 
-          <View style={{ marginTop: 20 }}>
-            {uiMenuItems.map((item, index) => (
-              <MenuItem
-                key={item.component}
-                {...item}
-                isFirst={index === 0}
-                isLast={index === uiMenuItems.length - 1}
-                icon={item.icon}
-                component={item.component}
-              />
-            ))}
-          </View>
+        <View style={{ marginTop: 20 }}>
+          {uiMenuItems.map((item, index) => (
+            <MenuItem
+              key={item.component}
+              {...item}
+              isFirst={index === 0}
+              isLast={index === uiMenuItems.length - 1}
+              icon={item.icon}
+              component={item.component}
+            />
+          ))}
+        </View>
 
-          <View style={{ marginTop: 20 }}>
-            {menuItems.map((item, index) => (
-              <MenuItem
-                key={item.component}
-                {...item}
-                isFirst={index === 0}
-                isLast={index === menuItems.length - 1}
-                icon={item.icon}
-                component={item.component}
-              />
-            ))}
-          </View>
-        </ScrollView>
-      </View>
-    </View>
+        <View style={{ marginTop: 20 }}>
+          {menuItems.map((item, index) => (
+            <MenuItem
+              key={item.component}
+              {...item}
+              isFirst={index === 0}
+              isLast={index === menuItems.length - 1}
+              icon={item.icon}
+              component={item.component}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </CustomView>
   );
 };
